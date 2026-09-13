9d9df68e8e6e3d86ffd61b9c861711ffb43e425b
taegyun <needle12s@gmail.com>
2026-09-13 22:26:17 +0900

    chore: Checkstyle 및 ArchUnit 검사 설정

--- apps/commerce-api/build.gradle.kts
+++ apps/commerce-api/build.gradle.kts
@@ -1,3 +1,14 @@
+plugins {
+    checkstyle
+}
+
+checkstyle {
+    toolVersion = "10.26.1"
+    configFile = rootProject.file("config/checkstyle/checkstyle.xml")
+    isIgnoreFailures = false
+    maxWarnings = 0
+}
+
 dependencies {
     // add-ons
     implementation(project(":modules:jpa"))
@@ -19,4 +30,5 @@ dependencies {
     // test-fixtures
     testImplementation(testFixtures(project(":modules:jpa")))
     testImplementation(testFixtures(project(":modules:redis")))
+    testImplementation("com.tngtech.archunit:archunit:1.5.0")
 }
